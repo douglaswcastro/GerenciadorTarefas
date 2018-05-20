6a059f5c161bbceffb3755679cda26fc2d26368e
douglaswcastro <douglaswcastro@hotmail.com>
2018-05-20 20:34:36 -0300

    commit

---
+++ GerenciadorTarefasTests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GerenciadorTarefas.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorTarefas.Controllers.Tests
+{
+    [TestClass()]
+    public class LoginControllerTests
+    {
+        [TestMethod()]
+        public void logInTest()
+        {
+            //Arrenge
+
+            //Act
+
+            //Assert
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void logOutTest()
+        {
+            //Arrenge
+
+            //Act
+
+            //Assert
+            Assert.Fail();
+        }
+    }
+}
